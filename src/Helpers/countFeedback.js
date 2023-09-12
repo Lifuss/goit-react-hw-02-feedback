463d8bace0ec614ed637d54d637fdc8f560fe0a3
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-09-12 16:14:38 +0300

    helpers propTypes

--- src/Helpers/countFeedback.js
+++ src/Helpers/countFeedback.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 export const countTotalFeedback = ({ good, bad, neutral }) => {
   const total = good + bad + neutral;
   return total;
@@ -12,4 +14,14 @@ export const countPositiveFeedbackPercentage = (good, total) => {
     return (positivePercentage = Math.ceil((good / total) * 100));
   }
   return positivePercentage;
+};
+
+countTotalFeedback.propTypes = {
+  good: PropTypes.number,
+  bad: PropTypes.number,
+  neutral: PropTypes.number,
+};
+countPositiveFeedbackPercentage.propTypes = {
+  good: PropTypes.number,
+  total: PropTypes.number,
 };
